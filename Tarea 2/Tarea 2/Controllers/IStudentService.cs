f53038d4122037ea0648d96796700a7108356f70
Hendrick Garcia Espinal <hendrick011@hotmail.com>
2019-07-11 01:40:35 -0400

    primer commit

---
+++ Tarea 2/Tarea 2/Controllers/IStudentService.cs
@@ -0,0 +1,12 @@
+﻿using Model;
+
+namespace Tarea_2.Controllers
+{
+    public interface IStudentService
+    {
+        object GetAll();
+        object Get(int id);
+        object Delete(int id);
+        object Add(Student model);
+    }
+}
